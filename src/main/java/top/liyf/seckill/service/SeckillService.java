678ee530896f3b2de3edd84b8b638ffcb332fe39
93814690 <93814690@qq.com>
2018-03-22 16:02:31 +0800

    service接口及其实现

---
+++ src/main/java/top/liyf/seckill/service/SeckillService.java
@@ -0,0 +1,38 @@
+package top.liyf.seckill.service;
+
+import top.liyf.seckill.dto.Exposer;
+import top.liyf.seckill.dto.SeckillExecution;
+import top.liyf.seckill.entity.Seckill;
+import top.liyf.seckill.exception.RepeatKillException;
+import top.liyf.seckill.exception.SeckillCloseException;
+import top.liyf.seckill.exception.SeckillException;
+
+import java.util.List;
+
+public interface SeckillService {
+
+    List<Seckill> getSeckillList();
+
+    Seckill getById(long seckillId);
+
+    /**
+     * 秒杀开启时输出秒杀接口地址，
+     * 否则输出系统时间和秒杀时间
+     *
+     * @param seckillId
+     * @return
+     */
+    Exposer exportSeckillUrl(long seckillId);
+
+    /**
+     *
+     * @param seckillID
+     * @param userPhone
+     * @param md5
+     * @return
+     * @throws SeckillException
+     * @throws RepeatKillException
+     * @throws SeckillCloseException
+     */
+    SeckillExecution executeSeckill(long seckillID, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException;
+}
---
+++ src/main/java/top/liyf/seckill/service/impl/SeckillServiceImpl.java
@@ -0,0 +1,98 @@
+package top.liyf.seckill.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.DigestUtils;
+import top.liyf.seckill.dao.SeckillDao;
+import top.liyf.seckill.dao.SuccessKilledDao;
+import top.liyf.seckill.dto.Exposer;
+import top.liyf.seckill.dto.SeckillExecution;
+import top.liyf.seckill.entity.Seckill;
+import top.liyf.seckill.entity.SuccessKilled;
+import top.liyf.seckill.enums.SeckillStateEnum;
+import top.liyf.seckill.exception.RepeatKillException;
+import top.liyf.seckill.exception.SeckillCloseException;
+import top.liyf.seckill.exception.SeckillException;
+import top.liyf.seckill.service.SeckillService;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author: liyf
+ * @Description:
+ * @Date: Created in 2018/3/22/ 11:18
+ * @Modified By:
+ */
+public class SeckillServiceImpl implements SeckillService {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private SeckillDao seckillDao;
+
+    private SuccessKilledDao successKilledDao;
+
+    //md5盐值字符串，用于混淆MD5
+    private final String salt = "sdfhuehf38jefn3*&(dsad212";
+
+
+    public List<Seckill> getSeckillList() {
+        return seckillDao.queryAll(0, 4);
+    }
+
+    public Seckill getById(long seckillId) {
+        return seckillDao.queryById(seckillId);
+    }
+
+    public Exposer exportSeckillUrl(long seckillId) {
+        Seckill seckill = seckillDao.queryById(seckillId);
+        if (seckill == null) {
+            return new Exposer(false, seckillId);
+        }
+        Date startTime = seckill.getStartTime();
+        Date endTime = seckill.getEndTime();
+        Date nowTime = new Date();
+        if (nowTime.getTime() < startTime.getTime() || nowTime.getTime() > endTime.getTime()) {
+            return new Exposer(false, seckillId, nowTime.getTime(), startTime.getTime(),
+                    endTime.getTime());
+        }
+        String md5 = getMD5(seckillId);
+        return new Exposer(true, md5, seckillId);
+    }
+
+    private String getMD5(long seckillId) {
+        String base = seckillId + "/" + salt;
+        String md5 = DigestUtils.md5DigestAsHex(base.getBytes());
+        return md5;
+    }
+
+    public SeckillExecution executeSeckill(long seckillID, long userPhone, String md5)
+            throws SeckillException, RepeatKillException, SeckillCloseException {
+        if (md5 == null || !md5.equals(getMD5(seckillID))) {
+            throw new SeckillException("seckill data rewrite");
+        }
+        //秒杀逻辑：减库存+记录购买行为
+        Date nowTime = new Date();
+        try {
+            int updateCount = seckillDao.reduceNumber(seckillID, nowTime);
+            if (updateCount <= 0) {
+                throw new SeckillCloseException("seckill is closed");
+            } else {
+                int insertCount = successKilledDao.insertSuccessKilled(seckillID, userPhone);
+                if (insertCount <= 0) {
+                    throw new RepeatKillException("seckill repeated");
+                } else {
+                    SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(seckillID, userPhone);
+                    return new SeckillExecution(seckillID, SeckillStateEnum.SUCCESS, successKilled);
+                }
+            }
+        } catch (SeckillCloseException e1) {
+            throw e1;
+        } catch (RepeatKillException e2) {
+            throw e2;
+        }catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new SeckillException("seckill inner error" + e.getMessage());
+        }
+    }
+}
