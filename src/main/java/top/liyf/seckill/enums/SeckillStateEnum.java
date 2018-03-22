e7b2297347cec964bbf1bcb975e9a85dc8d5b38b
93814690 <93814690@qq.com>
2018-03-22 16:03:31 +0800

    enum

---
+++ src/main/java/top/liyf/seckill/enums/SeckillStateEnum.java
@@ -0,0 +1,40 @@
+package top.liyf.seckill.enums;
+
+/**
+ * @Author: liyf
+ * @Description: 使用枚举表述常量字段
+ * @Date: Created in 2018/3/22/ 15:43
+ * @Modified By:
+ */
+public enum SeckillStateEnum {
+    SUCCESS(1, "秒杀成功"),
+    END(0, "秒杀结束"),
+    REPEAT_KILL(-1, "重复秒杀"),
+    INNER_ERROR(-2, "系统异常"),
+    FATA_REWRITE(-3, "数据篡改");
+
+    private int state;
+    private String stateInfo;
+
+    public int getState() {
+        return state;
+    }
+
+    public String getStateInfo() {
+        return stateInfo;
+    }
+
+    SeckillStateEnum(int state, String stateInfo) {
+        this.state = state;
+        this.stateInfo = stateInfo;
+    }
+
+    public static SeckillStateEnum stateOf(int index) {
+        for (SeckillStateEnum state : values()) {
+            if (state.getState() == index) {
+                return state;
+            }
+        }
+        return null;
+    }
+}
