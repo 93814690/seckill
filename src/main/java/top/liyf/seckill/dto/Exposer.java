abe50ad205f77a193c38207114ecb95c9d46a553
93814690 <93814690@qq.com>
2018-03-22 20:14:58 +0800

    修复拼写错误

--- src/main/java/top/liyf/seckill/dto/Exposer.java
+++ src/main/java/top/liyf/seckill/dto/Exposer.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class Exposer {
 
     //是否开启秒杀
-    private boolean seposed;
+    private boolean exposed;
 
     //id
     private String md5;
@@ -22,22 +22,22 @@ public class Exposer {
 
     private long end;
 
-    public Exposer(boolean seposed, String md5, long seckillId) {
-        this.seposed = seposed;
+    public Exposer(boolean exposed, String md5, long seckillId) {
+        this.exposed = exposed;
         this.md5 = md5;
         this.seckillId = seckillId;
     }
 
-    public Exposer(boolean seposed, long seckillId, long now, long start, long end) {
-        this.seposed = seposed;
+    public Exposer(boolean exposed, long seckillId, long now, long start, long end) {
+        this.exposed = exposed;
         this.seckillId = seckillId;
         this.now = now;
         this.start = start;
         this.end = end;
     }
 
-    public Exposer(boolean seposed, long seckillId) {
-        this.seposed = seposed;
+    public Exposer(boolean exposed, long seckillId) {
+        this.exposed = exposed;
         this.seckillId = seckillId;
     }
 }
